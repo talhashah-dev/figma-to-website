0daafbcc25e0cd1b4ad8aded08898ebf413e3a5a
Talha Shah <syedt.in00@gmail.com>
2024-05-05 23:10:45 +0500

    Header Styling Completed - SCSS to CSS Chagned
    Website Header Components styling Completed and Changed styling files from scss to css

--- SoundDZign/src/components/Header/Header.jsx
+++ SoundDZign/src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import "./Header.css";
+
+// eslint-disable-next-line no-unused-vars
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="wrapper">
+        <div className="hero-text">
+          <p className="course-name">Sound Design Masterclass</p>
+          <h1>Learn the Art of Sound Design</h1>
+          <button className="demo-btn">Demo Lesson</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
