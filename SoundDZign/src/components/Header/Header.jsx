a1dd486c16399211ac9e7f29bc9bc8ea63fdaf41
Talha Shah <syedt.in00@gmail.com>
2024-05-06 13:26:50 +0500

    Sound DZign - Navbar Styling Completed

--- SoundDZign/src/components/Header/Header.jsx
+++ SoundDZign/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./Header.css";
+import Navbar from "../Navbar/Navbar";
 
 // eslint-disable-next-line no-unused-vars
 
@@ -8,6 +9,7 @@ function Header() {
   return (
     <header>
       <div className="wrapper">
+        <Navbar />
         <div className="hero-text">
           <p className="course-name">Sound Design Masterclass</p>
           <h1>Learn the Art of Sound Design</h1>
--- SoundDZign/src/components/Navbar/Navbar.jsx
+++ SoundDZign/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <a href="" className="logo">
+        Sound<span>DZign</span>
+      </a>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">CourseDetails</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Testimonials</a></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
